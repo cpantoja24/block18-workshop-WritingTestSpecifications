d97ae30fb0f368cbb22f5b202dcffc9275b74de6
Cheyenne Pantoja <Cheyenne@MBP-5000.frontiernet.net>
2023-11-22 09:06:41 -0800

    added funtional tests

--- index.js
+++ index.js
@@ -1,18 +1,46 @@
 // Unit Tests:
-// A function called "multiplication" that returns the product of the two input numbers.
-    // Expect multiplication(x,y) to be a number
-    // 
+// 1. A function called "multiplication" that returns the product of the two input numbers.
 
-// A function called "concatOdds" takes two arrays of integers as arguments. It should return a single array that only contains the odd numbers, in ascending order, from both of the arrays.
+    // Expect multiplication(x,y) to be a number
+    // Expect multiplication(x,y) to be a product of two numbers
+    // Expect multiplication(x,y) to have two arguments 
+    // Expect multiplication(x,y) to return undefined if arguments are not numbers or only one number is provided
+
+
+// 2. A function called "concatOdds" takes two arrays of integers as arguments. It should return a single array that only contains the odd numbers, in ascending order, from both of the arrays.
 // Example: concatOdds([3, 2, 1], [9, 1, 1, 1, 4, 15, -1])
 // ...should result in [-1, 1, 3, 9, 15]
-    // Expect concatOdds to be an array 
-    // Expect concatOdds to contain odd numbers in ascending order
-
 // Think about the following; you may need to make assumptions or decisions about the prompt and how the code should behave:
-
 // What should happen with unexpected inputs?
-
 // What kinds of unexpected inputs should we worry about?
-
 // What should happen when there are multiples of the same odd number in the arrays? (Hint: We gave you the answer to this one in the example above.)
+
+    // Expect concatOdds to be an array 
+    // Expect concatOdds to take two arrays as arguments
+    // Expect concatOdds to combine two arrays
+    // Expect concatOdds to be odd numbers from both arrays
+    // Expect concatOdds to be in ascending order
+    // Expect concatOdds to only show one of each odd number
+    // Expect concatOdds to return undefined if arguments are not arrays or there is only one array provided
+
+// Functional Tests:
+// 1. A shopping cart checkout feature that allows a user to check out as a guest (without an account), or as a logged-in user. They should be allowed to do either, but should be asked if they want to create an account or log in if they check out as a guest.
+// Think about the following; you may need to make assumptions or decisions about the prompt and how the feature should behave:
+// What should happen if the cart is empty?
+// What needs to be shown to the user at each step of check out?
+// What behaviors of this feature does the prompt miss or gloss over?
+// Hint: Observe the shopping cart and checkout features of some popular websites to get some ideas!
+
+    // When a user is checking out, they should be asked if they have an account or want to check out as a guest
+    // When a user is checking out, they should be asked to sign into their account if they have one
+    // When a user is checking out, they should be able to reset their password if they already have an account
+    // When a user is checking out, they should be shown an incorrect information popup if the username and password are incorrect
+    // When a user is checking out, they should be asked to if they want to create an account if they are checking out as a guest 
+    // When a user is checking out, they should be prompted to add things to their cart if the cart is empty
+    // When a user is checking out, they should be shown the items and quantities in their cart with the prices of each
+    // When a user is checking out, thay should be shown to subtotal of the items in their cart
+    // When a user is checking out, they should be shown the taxes of the items in their cart
+    // When a user is checking out, they should be shown the deilvery fee of the items in their cart
+    // When a user is checking out, they should be shown the total of their order
+    
+    
