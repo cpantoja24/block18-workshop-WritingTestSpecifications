796bb3389afdec674d43ed6e2812e74a87f2ce97
Cheyenne Pantoja <Cheyenne@MBP-5000.frontiernet.net>
2023-11-22 08:28:16 -0800

    started on unit tests

---
+++ index.js
@@ -0,0 +1,18 @@
+// Unit Tests:
+// A function called "multiplication" that returns the product of the two input numbers.
+    // Expect multiplication(x,y) to be a number
+    // 
+
+// A function called "concatOdds" takes two arrays of integers as arguments. It should return a single array that only contains the odd numbers, in ascending order, from both of the arrays.
+// Example: concatOdds([3, 2, 1], [9, 1, 1, 1, 4, 15, -1])
+// ...should result in [-1, 1, 3, 9, 15]
+    // Expect concatOdds to be an array 
+    // Expect concatOdds to contain odd numbers in ascending order
+
+// Think about the following; you may need to make assumptions or decisions about the prompt and how the code should behave:
+
+// What should happen with unexpected inputs?
+
+// What kinds of unexpected inputs should we worry about?
+
+// What should happen when there are multiples of the same odd number in the arrays? (Hint: We gave you the answer to this one in the example above.)
